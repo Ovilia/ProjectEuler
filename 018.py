9c639677e5957cafae9e201b1a6d6a8d9e47cc73
ovilia <zwl.sjtu@gmail.com>
2013-10-20 19:46:12 +0800

    18 and 67, with the same code but different input

---
+++ 018.py
@@ -0,0 +1,11 @@
+infile = open('018.in')
+lines = []
+for line in infile:
+    lines.append([int(i) for i in line[:-1].split(' ')])
+
+route = lines[-1]
+for i in range(len(lines) - 1, 0, -1):
+    for j in range(i):
+        route[j] = max(route[j], route[j + 1]) + lines[i - 1][j]
+
+print route[0]
